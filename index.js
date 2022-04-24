092ecec6ff57913349f8887710e825ddee7162b2
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-24 23:39:12 +0600

    load data from database and apply pagination functionality

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+// MongoDB integration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrahu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db("emaJohn").collection("product");
+
+        // GET: Load dat from db to server
+        app.get('/product', async(req, res)=>{
+            // console.log("Query", req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+
+            let products;
+            if(page || size){
+                products = await cursor.skip(page*size).limit(size).toArray();                
+            } else{
+                products = await cursor.toArray();
+            }
+            res.send(products); 
+        })
+
+        // GET: for pagination
+        app.get('/productCount', async(req, res)=>{
+            // const query = {};
+            // const cursor = productCollection.find(query);
+            // const count = await cursor.count();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count});
+        })
+
+        // POST: use post to get products by ids
+        app.post('/productByKeys', async(req, res)=>{
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = {_id: {$in: ids}};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            // console.log(keys);
+            res.send(products);
+        })
+    } finally{
+
+    }
+}
+run().catch(console.dir);
+
+// root API
+app.get('/', (req, res)=>{
+    res.send("Ema John Server running successfully !!!")
+})
+
+app.listen(port, ()=>{
+    console.log("Listening the server from port :", port)
+})
